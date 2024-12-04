e07d6caf2e3e04c1a82dd71c5c5a7bc59f38c975
Devam Kumar <118193504+DevamKumar25@users.noreply.github.com>
2024-12-04 16:56:26 +0530

    Create App.jsx

---
+++ HOOKS/useReducer/App.jsx
@@ -0,0 +1,15 @@
+// /src/App.js
+
+import React from 'react';
+import Counter from './Components/Counter';
+
+const App = () => {
+  return (
+    <div>
+      <h1>React Counter with useReducer</h1>
+      <Counter />
+    </div>
+  );
+};
+
+export default App;
